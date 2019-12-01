3df61c0bc86d6bcf44ea10793d9e0c1c9e79e39b
Maryna Cherniavska <maryna.cherniavska@olx.com>
2019-12-01 15:26:16 +0100

    added alternative JSON output with the start time

--- app.js
+++ app.js
@@ -6,22 +6,47 @@ const API_PATH = "/kabachok/rock";
 const app = express();
 
 let rockStatus = "stopped";
+let timeoutObject = null;
+let startTime = 0;
+
+const sendStatusResponse = (res) => {
+    res.format({
+        text: function () {
+            res.send(rockStatus);
+        },
+        json: function () {
+            res.json({status: `${rockStatus}`, since: startTime});
+        }
+    });
+
+};
 
 const sendStart = (res) => {
     console.log(`Starting cradle...`);
+    if (!startTime) {
+        startTime = new Date().getTime();
+    }
     rockStatus = "started";
     if (!!res) {
-        res.send(rockStatus);
+        sendStatusResponse(res);
     }
 };
 
 const sendStop = (res) => {
     console.log(`Stopping cradle...`);
     rockStatus = "stopped";
+    startTime = 0;
     if (!!res) {
-        res.send(rockStatus);
+        sendStatusResponse(res);
     } else {
         console.log(`Cradle stopped - timed out`);
+    }
+};
+
+const sendStartTime = (res) => {
+    console.log(`Start time requested...`);
+    if (!!res) {
+        sendStatusResponse(res);
     }
 };
 
@@ -32,17 +57,25 @@ app.listen(PORT, () => {
 app.route(API_PATH)
     .get((req, res) => {
         console.log(`Sending status: ${rockStatus}`);
-        res.send(rockStatus);
+        sendStatusResponse(res);
     });
 
 // for simplicity - gets
 app.route(`${API_PATH}/start`)
     .get((req, res) => {
         sendStart(res);
-        setTimeout(sendStop, DEFAULT_DURATION, null);
+        if (timeoutObject) {
+            clearTimeout(timeoutObject);
+        }
+        timeoutObject = setTimeout(sendStop, DEFAULT_DURATION, null);
     });
 
 app.route(`${API_PATH}/stop`)
     .get((req, res) => {
         sendStop(res);
+    });
+
+app.route(`${API_PATH}/since`)
+    .get((req, res) => {
+        sendStartTime(res);
     });
